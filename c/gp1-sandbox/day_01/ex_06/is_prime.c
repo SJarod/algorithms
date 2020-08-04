10a10d48c90f847bc7fc3e50a618c66df271f4e3
jarod.sengkeo <j.sengkeo@student.isartdigital.com>
2020-08-04 16:39:46 +0200

    day1 ex6

---
+++ c/gp1-sandbox/day_01/ex_06/is_prime.c
@@ -0,0 +1,26 @@
+#include "is_prime.h"
+
+#include <stdio.h>
+
+void my_put_char(char c)
+{
+	fwrite(&c,sizeof(char),1,stdout);
+}
+
+void is_prime(unsigned int num)
+{
+	int prime = 1;
+	for (unsigned int i = 2; i < num; ++i)
+	{
+		if (num % i == 0)
+		{
+			prime = 0;
+			break;
+		}
+	}
+
+	if (prime == 0)
+		my_put_char('N');
+	else
+		my_put_char('Y');
+}
---
+++ c/gp1-sandbox/day_01/ex_06/is_prime.h
@@ -0,0 +1,7 @@
+#ifndef __IS_PRIME__
+#define __IS_PRIME__
+
+void my_put_char(char c);
+void is_prime(unsigned int num);
+
+#endif
