6cff3ab0029e373363bd23cf174a53d4449fe96f
jarod.sengkeo <j.sengkeo@student.isartdigital.com>
2020-08-06 18:31:54 +0200

    day2 ex9

--- c/gp1-sandbox/day_02/ex_09/string_compare.c
+++ c/gp1-sandbox/day_02/ex_09/string_compare.c
@@ -15,7 +15,27 @@ int string_compare(char const* str1, char const* str2)
 		else if (length1 > length2)
 			return 1;
 		else
-			return 0;
+		{
+			int count1 = 0;
+			int count2 = 0;
+
+			for (unsigned int i = 0; i < string_length(str1); ++i)
+			{
+				count1 += str1[i];
+			}
+
+			for (unsigned int j = 0; j < string_length(str2); ++j)
+			{
+				count2 += str2[j];
+			}
+
+			if (count1 < count2)
+				return -1;
+			else if (count1 > count2)
+				return 1;
+			else
+				return 0;
+		}
 	}
 	return 0;
 }
