5d67aeb0902a92de405b2a61279033bcd50c1faf
jarod.sengkeo <j.sengkeo@student.isartdigital.com>
2020-08-11 14:03:38 +0200

    day4 ex1

---
+++ c/gp1-sandbox/day_04/ex_01/enemy.c
@@ -0,0 +1,58 @@
+#include "enemy.h"
+#include "my_put_string.h"
+#include "string_length.h"
+
+#include <stdlib.h>
+
+void enemy_construct(struct s_enemy* enemy, int l, int a, char* n, char* c)
+{
+	if (enemy == 0)
+		return;
+	if (n == 0)
+		return;
+	if (c == 0)
+		return;
+
+	enemy->name = malloc(string_length(n) * sizeof(char));
+	enemy->cry = malloc(string_length(c) * sizeof(char));
+
+	enemy->life = l;
+	enemy->attack = a;
+
+	for (unsigned int i = 0; i < string_length(n); ++i)
+	{
+		enemy->name[i] = n[i];
+	}
+
+	for (unsigned int i = 0; i < string_length(c); ++i)
+	{
+		enemy->cry[i] = c[i];
+	}
+	
+	my_put_string(enemy->name);
+	my_put_string(" is born.\n");
+}
+
+void enemy_cry(const t_enemy* enemy)
+{
+	if (enemy == 0)
+		return;
+
+	my_put_string(enemy->name);
+	my_put_string(": ");
+	my_put_string(enemy->cry);
+	my_put_string("\n");
+}
+
+void enemy_destruct(struct s_enemy* enemy)
+{
+	if (enemy == 0)
+		return;
+
+	my_put_string(enemy->name);
+	my_put_string(" died.");
+	my_put_string("\n\0");
+
+	free(enemy->name);
+	free(enemy->cry);
+}
