cf4ff65b51bff0ea91844fa2574a7b81763236f3
jarod.sengkeo <j.sengkeo@student.isartdigital.com>
2020-08-12 09:56:30 +0200

    day4 ex3

---
+++ c/gp1-sandbox/day_04/ex_03/my_put_string.c
@@ -0,0 +1,21 @@
+#include "my_put_string.h"
+#include "my_put_char.h"
+
+#include <stdio.h>
+
+//void my_put_char(char c)
+//{
+//	fwrite(&c, sizeof(char), 1, stdout);
+//}
+
+void my_put_string(char const* str)
+{
+	if (str != NULL)
+	{
+		while (*str != '\0')
+		{
+			my_put_char(*(str));
+			++str;
+		}
+	}
+}
