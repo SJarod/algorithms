5b32ca49bacbf5e00365707739867ab29201709c
jarod.sengkeo <j.sengkeo@student.isartdigital.com>
2020-08-12 15:44:44 +0200

    day4 ex4

--- c/gp1-sandbox/day_04/ex_04/operation.h
+++ c/gp1-sandbox/day_04/ex_04/operation.h
@@ -4,7 +4,7 @@
 int			addition(int a, int b);
 int			substraction(int a, int b);
 int			multiplication(int a, int b);
-int			divistion(int a, int b);
+int			division(int a, int b);
 
 typedef int		(*pointer_operation_type)(int a, int b);	//function pointer
 
