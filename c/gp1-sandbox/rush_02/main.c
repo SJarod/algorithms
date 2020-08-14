6a6ccfbf95b4a5d0a882b10814115deee42702cb
jarod.sengkeo <j.sengkeo@student.isartdigital.com>
2020-08-14 19:02:12 +0200

    rush2

--- c/gp1-sandbox/rush_02/main.c
+++ c/gp1-sandbox/rush_02/main.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-	/*t_float const num1 = create_float("73281236478210010547369549852147", 7);
+	t_float const num1 = create_float("73281236478210010547369549852147", 7);
 	t_float const num2 = create_float("654147863214589743215478", 4);
 	t_float num3;
 	print_float(&num1);
@@ -14,27 +14,27 @@ int main()
 	add_digit_right(&num3, 5);
 	add_digit_left(&num3, 9);
 
-	reset_float(&num3);*/
+	reset_float(&num3);
 
-	/*t_float flottant;
+	t_float flottant;
 	init_from_string(&flottant, "123,456");
 	print_float(&flottant);
 
 	//add_digit_left(&flottant, 9);
 	//add_digit_right(&flottant, 3);
 
-	reset_float(&flottant);*/
+	reset_float(&flottant);
 
-	t_float num1;
-	t_float num2;
-	t_float num3;
-	init_from_string(&num1, "7328123647821001054734954,9852147");
-	init_from_string(&num2, "65414786321458974321,5478");
-	add_floats(&num1, &num2, &num3);
+	t_float num4;
+	t_float num5;
+	t_float num6;
+	init_from_string(&num4, "7328123647821001054734954,9852147");
+	init_from_string(&num5, "65414786321458974321,5478");
+	add_floats(&num4, &num5, &num6);
 
-	reset_float(&num1);
-	reset_float(&num2);
-	reset_float(&num3);
+	reset_float(&num4);
+	reset_float(&num5);
+	reset_float(&num6);
 
 	return 0;
 }
