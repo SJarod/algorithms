d6061b8bd7308004170b03b089e4c7c6c0d1c032
jarod.sengkeo <j.sengkeo@student.isartdigital.com>
2020-08-17 10:33:50 +0200

    day5 ex1

--- c/gp1-sandbox/day_05/ex_01/power_it.c
+++ c/gp1-sandbox/day_05/ex_01/power_it.c
@@ -2,13 +2,18 @@
 
 int power_it(int base, unsigned int pow)
 {
+	if (pow == 0)
+		return 1;
+
 	if (pow <= 1)
 		return base;
 
+	int result = base;
+
 	for (unsigned int i = 0; i < pow - 1; ++i)
 	{
-		base *= base;
+		result *= base;
 	}
 
-	return base;
+	return result;
 }
--- c/gp1-sandbox/day_05/ex_01/power_rec.c
+++ c/gp1-sandbox/day_05/ex_01/power_rec.c
@@ -2,7 +2,11 @@
 
 int power_rec(int base, unsigned int pow)
 {
+	if (pow == 0)
+		return 1;
+
 	if (pow <= 1)
 		return base;
+
 	return base * power_rec(base, pow - 1);
 }
