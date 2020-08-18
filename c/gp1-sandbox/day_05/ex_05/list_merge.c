01f00c8c238a67d04fca9d918bbe142e32aae0bb
jarod.sengkeo <j.sengkeo@student.isartdigital.com>
2020-08-18 15:06:36 +0200

    day5 ex5

--- c/gp1-sandbox/day_05/ex_05/list_merge.c
+++ c/gp1-sandbox/day_05/ex_05/list_merge.c
@@ -15,7 +15,7 @@ t_list* list_merge(t_list* list1, t_list* list2)
 
 	for (int j = 0; j < list2->size; ++j)
 	{
-		whichNode = list2->size - j;
+		whichNode = list2->size - j - 1;
 		temp = list2->head;
 
 		while (whichNode > 0)
@@ -29,7 +29,7 @@ t_list* list_merge(t_list* list1, t_list* list2)
 
 	for (int j = 0; j < list1->size; ++j)
 	{
-		whichNode = list1->size - j;
+		whichNode = list1->size - j - 1;
 		temp = list1->head;
 
 		while (whichNode > 0)
@@ -40,6 +40,10 @@ t_list* list_merge(t_list* list1, t_list* list2)
 
 		list_insert_next(newList, NULL, list_node_data(temp));
 	}
+
+	list_destroy(&list1);
+	list_destroy(&list2);
+	list_foreach(newList, newList->destroy);
 
 	return newList;
 }
