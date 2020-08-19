dba337ff2c5075567bba09913e9844f891e29240
jarod.sengkeo <j.sengkeo@student.isartdigital.com>
2020-08-19 15:46:53 +0200

    day5 ex5

--- c/gp1-sandbox/day_05/ex_05/list_merge.c
+++ c/gp1-sandbox/day_05/ex_05/list_merge.c
@@ -9,10 +9,19 @@ t_list* list_merge(t_list* list1, t_list* list2)
 	if (list2 == NULL)
 		return NULL;
 
-	list1->tail->next = list2->head;
-	list1->size += list2->size;
-	list1->tail = list2->tail;
-	list1->tail->next = NULL;
+	t_list* newList = list_create();
 
-	return list1;
+	newList->head = list1->head;
+	list1->tail->next = list2->head;
+	newList->size = list1->size + list2->size;
+	newList->tail = list2->tail;
+	newList->tail->next = NULL;
+	list1->head = NULL;
+	list2->head = NULL;
+	list1->tail = NULL;
+	list2->tail = NULL;
+	list1->size = 0;
+	list2->size = 0;
+
+	return newList;
 }
