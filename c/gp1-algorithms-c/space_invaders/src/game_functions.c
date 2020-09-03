43fb819652c790f87eb00659ceabf08ebb7afbd8
Jarod <j.sengkeo@student.isartdigital.com>
2020-09-03 12:10:44 +0200

    invaders

--- c/gp1-algorithms-c/space_invaders/src/game_functions.c
+++ c/gp1-algorithms-c/space_invaders/src/game_functions.c
@@ -38,40 +38,68 @@ void move_player(game_t* game, const game_inputs_t* inputs)
     if (inputs->buttons[BUTTON_RIGHT])
         game->player.x += inputs->deltaTime * PLAYER_SPEED;
     
-    game->player.x = clamp(game->player.x, 10, 100);
+    game->player.x = clamp(game->player.x, 10, game->width - 10);
 }
 
 // playerBullet is set to the player position and set alive
 void player_shoot(game_t* game)
 {
-
+	game->playerBulletAlive = true;
+	game->playerBullet.x = game->player.x;
+	game->playerBullet.y = game->player.y;
 }
 
 // Each enemies are moved using enemyDirection and enemySpeed
 // If an enemy reach the border of the screen, the direction is changed the next frame
 void move_enemies(game_t* game, float deltaTime)
 {
+	for (int i = 0; i < game->enemyCount; ++i)
+	{
+		game->enemies[i].x += deltaTime * game->enemySpeed * game->enemyDirection;
+	}
 
+	for (int i = 0; i < game->enemyCount; ++i)
+	{
+		if (game->enemies[i].x <= 10 ||
+		    game->enemies[i].x >= game->width - 10)
+		{
+			game->enemyDirection = -game->enemyDirection;
+
+			for (int j = 0; j < MAX_ENEMIES; ++j)
+			{
+				game->enemies[j].y += 1;
+			}
+
+			break;
+		}
+	}
 }
 
 // A random enemy is selected to fire a bullet
 // A new bullet is added to the list of bullets
 void random_enemy_fire(game_t* game)
 {
-
+	int i = rand() % game->enemyCount;
+	++game->enemyBulletCount;
+	game->enemyBullets[game->enemyBulletCount - 1].x = game->enemies[i].x;
+	game->enemyBullets[game->enemyBulletCount - 1].y = game->enemies[i].y;
 }
 
 // Move the single player bullet
 // Set playerBulletAlive to false if bullet is out of bound
 void move_player_bullet(game_t* game, float deltaTime)
 {
+	game->playerBullet.y -= 100 * deltaTime;
 
+	if (game->playerBullet.y > 0)
+		game->player.canShoot = false;
 }
 
 // Remove enemy from enemies
 void remove_enemy(game_t* game, int enemyIndex)
 {
-
+	--game->enemyCount;
+	game->enemies[enemyIndex] = game->enemies[game->enemyCount];
 }
 
 // Remove enemy bullet from enemyBullets
@@ -84,7 +112,7 @@ void remove_enemy_bullet(game_t* game, int enemyBulletIndex)
 // Remove the bullet if out of range
 void move_enemies_bullets(game_t* game, float deltaTime)
 {
-
+	game->enemyBullets[game->enemyBulletCount].y += 100 * deltaTime;
 }
 
 // Check if the player is hit by a bullet using bullet position and PLAYER_WIDTH/PLAYER_HEIGHT
@@ -96,5 +124,11 @@ bool player_is_hit_by_bullet(player_t* player, bullet_t* bullet)
 // Check if the enemy is hit by a bullet using bullet position and ENEMY_WIDTH/ENEMY_HEIGHT
 bool enemy_is_hit_by_bullet(enemy_t* enemy, bullet_t* bullet)
 {
+	if (bullet->x >= enemy->x - ENEMY_WIDTH / 2 && bullet->x <= enemy->x + ENEMY_WIDTH / 2 &&
+	    bullet->y <= enemy->y + ENEMY_HEIGHT / 2 && bullet->y >= enemy->y - ENEMY_HEIGHT / 2)
+	{
+		return true;
+	}
+
     return false;
 }
