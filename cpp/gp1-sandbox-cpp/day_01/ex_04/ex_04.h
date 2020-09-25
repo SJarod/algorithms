f5746b1e40f8426ffad954c464b4df7f35682493
U-ISART\j.sengkeo <j.sengkeo@Zotac-GP-08.isart.lan>
2020-09-25 13:09:09 +0200

    ex4

---
+++ cpp/gp1-sandbox-cpp/day_01/ex_04/ex_04.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace day_01::ex_04
+{
+    enum class test_enum
+    {
+        my_name = 1
+    };
+
+    enum class first_enum
+    {
+        my_name = 'c'
+    };
+
+    enum class second_enum
+    {
+        my_name = 4u
+    };
+}
