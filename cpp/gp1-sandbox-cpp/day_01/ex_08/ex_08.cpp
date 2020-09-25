ac4a9c730063a1d9c8f2d5e1604053e7f6ddd4d3
U-ISART\j.sengkeo <j.sengkeo@Zotac-GP-08.isart.lan>
2020-09-25 14:36:10 +0200

    ex8

---
+++ cpp/gp1-sandbox-cpp/day_01/ex_08/ex_08.cpp
@@ -0,0 +1,28 @@
+#include "ex_08.h"
+
+#include <iostream>
+
+void day_01::ex_08::print(const char* str)
+{
+    std::cout << str << std::endl;
+}
+
+void day_01::ex_08::print(int num)
+{
+    std::cout << num << std::endl;
+}
+
+void day_01::ex_08::print(unsigned int numu)
+{
+    std::cout << numu << std::endl;
+}
+
+void day_01::ex_08::print(double numd)
+{
+    std::cout << numd << std::endl;
+}
+
+void day_01::ex_08::print(float numf)
+{
+    std::cout << numf << std::endl;
+}
