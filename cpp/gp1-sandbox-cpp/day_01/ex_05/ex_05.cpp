04683d0cd15bf3bd8f0beb1d1dbc627102d9937a
U-ISART\j.sengkeo <j.sengkeo@Zotac-GP-08.isart.lan>
2020-09-25 13:41:14 +0200

    ex5

---
+++ cpp/gp1-sandbox-cpp/day_01/ex_05/ex_05.cpp
@@ -0,0 +1,8 @@
+#include "ex_05.h"
+
+#include <cassert>
+
+void day_01::ex_05::is_nullptr(void const* ptr)
+{
+    assert(ptr != nullptr);
+}
---
+++ cpp/gp1-sandbox-cpp/day_01/ex_05/ex_05.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace day_01::ex_05
+{
+    void is_nullptr(void const* ptr);
+}
