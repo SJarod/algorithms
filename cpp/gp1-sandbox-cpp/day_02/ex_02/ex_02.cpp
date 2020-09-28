461372456c16dbf257333287a7f9fefb03a005a1
U-ISART\j.sengkeo <j.sengkeo@Zotac-GP-08.isart.lan>
2020-09-28 09:40:13 +0200

    day2 ex2

---
+++ cpp/gp1-sandbox-cpp/day_02/ex_02/ex_02.cpp
@@ -0,0 +1,13 @@
+#include "ex_02.h"
+
+#include <iostream>
+
+day_02::ex_02::sloth::sloth()
+{
+    std::cout << "Sloth is alive !" << std::endl;
+}
+
+day_02::ex_02::sloth::~sloth()
+{
+    std::cout << "Sloth died! Why did you do that?!" << std::endl;
+}
---
+++ cpp/gp1-sandbox-cpp/day_02/ex_02/ex_02.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace day_02::ex_02
+{
+    class sloth
+    {
+        public:
+            sloth();
+            ~sloth();
+    };
+}
