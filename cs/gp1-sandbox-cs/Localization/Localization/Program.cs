c569a69510e40e62c382004d3c43614ea96fc46f
U-ISART\j.sengkeo <j.sengkeo@Zotac-GP-08.isart.lan>
2021-02-17 16:32:01 +0100

    day1 ex123

---
+++ cs/gp1-sandbox-cs/Localization/Localization/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.IO;
+using System.Text;
+
+namespace Localization
+{
+    class LanguageConfig
+    {
+        List<string> language = new List<string>();
+        List<string> keyword = new List<string>();
+
+        void getLanguages(StreamReader file)
+        {
+            string str = file.ReadLine();
+            string[] splitStr = str.Split('|');
+
+            for (int i = 0; i < splitStr.Length; ++i)
+            {
+                language.Add(splitStr[i]);
+            }
+        }
+
+        void getKeyWords(StreamReader file)
+        {
+            string str = file.ReadLine();
+            string[] splitStr = str.Split('|');
+
+            for (int i = 0; i < splitStr.Length; ++i)
+            {
+                keyword.Add(splitStr[i]);
+            }
+        }
+
+        public void initDictionary()
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, @"..\..\..\dictionary\config.txt");
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("could not open config file " + path);
+                return;
+            }
+
+            StreamReader file = new StreamReader(path);
+
+            getLanguages(file);
+            getKeyWords(file);
+
+            file.Close();
+            file.Dispose();
+        }
+    }
+
+    class Language
+    {
+        string language;
+
+        Dictionary<string, string> word = new Dictionary<string, string>();
+
+        public Language(string l)
+        {
+            language = l;
+
+            string path = Path.Combine(Environment.CurrentDirectory, @"..\..\..\dictionary\" + language + ".txt");
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("could not open config file " + path);
+                return;
+            }
+
+            StreamReader file = new StreamReader(path);
+
+            getWords(file);
+
+            file.Close();
+            file.Dispose();
+        }
+
+        void getWords(StreamReader file)
+        {
+            for (int i = 0; i < 3; ++i)
+            {
+                string str = file.ReadLine();
+                string[] splitStr = str.Split('|');
+
+                word.Add(splitStr[0], splitStr[1]);
+            }
+        }
+
+        public string getName()
+        {
+            return language;
+        }
+
+        public string getContent(string key)
+        {
+            if (word.ContainsKey(key))
+                return word[key];
+
+            return "no keyword " + key + " found in " + language;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool windowShouldClose = false;
+
+            LanguageConfig config = new LanguageConfig();
+            config.initDictionary();
+
+            Language english = new Language("english");
+
+            while (!windowShouldClose)
+            {
+                Console.WriteLine("Make a choice");
+                Console.WriteLine("Current dictionary : " + english.getName());
+                Console.WriteLine("");
+                Console.WriteLine("1) Select a dictionary");
+                Console.WriteLine("2) Create a dictionary");
+                Console.WriteLine("3) Display dictionary content");
+                Console.WriteLine("4) Get a localisation");
+                Console.WriteLine("5) Add a localisation");
+                Console.WriteLine("6) Remove a localisation");
+                Console.WriteLine("7) Save");
+                Console.WriteLine("8) Exit");
+
+                string s = Console.ReadLine();
+                switch (s)
+                {
+                    case "1":
+                        {
+                            Console.Clear();
+                            break;
+                        }
+                    case "2":
+                        {
+                            Console.Clear();
+                            break;
+                        }
+                    case "3":
+                        {
+                            Console.Clear();
+                            break;
+                        }
+                    case "4":
+                        {
+                            Console.Clear();
+                            break;
+                        }
+                    case "5":
+                        {
+                            Console.Clear();
+                            break;
+                        }
+                    case "6":
+                        {
+                            Console.Clear();
+                            break;
+                        }
+                    case "7":
+                        {
+                            Console.Clear();
+                            break;
+                        }
+                    case "8":
+                        {
+                            windowShouldClose = true;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("unavailable option");
+                            Console.WriteLine("");
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ cs/gp1-sandbox-cs/csharp/csharp/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharp
+{
+    struct Vector2
+    {
+        public double x, y;
+
+        public Vector2(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double Magnitude()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        public override string ToString()
+        {
+            string str;
+
+            str = "x : " + x.ToString() + ", y : " + y.ToString();
+
+            return str;
+        }
+    }
+
+    class Vector2MagCompare : IComparer<Vector2>
+    {
+        public int Compare(Vector2 x, Vector2 y)
+        {
+            double xmag = x.Magnitude(), ymag = y.Magnitude();
+
+            if (xmag > ymag)
+                return 1;
+            else if (xmag < ymag)
+                return -1;
+            else
+                return 0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                // InvertStr
+                // ---------
+                string a = "Hello World!";
+                InvertStr(ref a);
+                Console.WriteLine(a);
+            }
+
+            {
+                // Swap
+                // ----
+                dynamic a = 10;
+                dynamic b = 4;
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                Swap(ref a, ref b);
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+            }
+
+            {
+                // Sort List
+                // ---------
+                List<Vector2> myList = new List<Vector2>();
+                for (int i = 0; i < 10; ++i)
+                {
+                    Random rand = new Random();
+                    myList.Add(new Vector2(rand.NextDouble(), rand.NextDouble()));
+                }
+
+                for (int i = 0; i < 10; ++i)
+                {
+                    Console.WriteLine(myList[i].ToString() + " , mag : " + myList[i].Magnitude());
+                }
+
+                myList.Sort(new Vector2MagCompare());
+                Console.WriteLine("sort");
+
+                for (int i = 0; i < 10; ++i)
+                {
+                    Console.WriteLine(myList[i].ToString() + " , mag : " + myList[i].Magnitude());
+                }
+            }
+        }
+
+        static string InvertStr(string str)
+        {
+            string result = "";
+
+            for (int i = str.Length; i > 0; --i)
+            {
+                result += str.ToCharArray()[i - 1];
+            }
+
+            return result;
+        }
+
+        static void InvertStr(ref string str)
+        {
+            string temp = str;
+            str = "";
+
+            for (int i = temp.Length; i > 0; --i)
+            {
+                str += temp.ToCharArray()[i - 1];
+            }
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int c = b;
+            b = a;
+            a = c;
+        }
+
+        static void Swap(ref dynamic a, ref dynamic b)
+        {
+            dynamic c = b;
+            b = a;
+            a = c;
+        }
+
+        static void printList(List<dynamic> list)
+        {
+            for (int i = 0; i < list.Count; ++i)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+    }
+}
