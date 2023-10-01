bdedb01d7f77f6aa97338409b5cdae86191ba192
SJarod <91637364+SJarod@users.noreply.github.com>
2023-10-01 23:45:07 +0200

    random

---
+++ cpp/rand.hpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+
+template<typename T>
+inline T random(T a, T b)
+{
+    throw std::runtime_error("implementation");
+}
+
+template<>
+inline int random<int>(int a, int b)
+{
+    return a + (std::rand()) / ((RAND_MAX / (b + 1 - a)));
+}
+
+template<>
+inline float random<float>(float a, float b)
+{
+    return a + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (b - a)));
+}
