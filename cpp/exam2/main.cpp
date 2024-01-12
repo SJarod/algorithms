184fc2972f9039181373c2fae266a2fc9ef8ca5f
Jarod <j.sengkeo@student.isartdigital.com>
2024-01-12 15:50:16 +0100

    exercice 2

--- cpp/exam2/main.cpp
+++ cpp/exam2/main.cpp
@@ -6,8 +6,11 @@
 
 int main()
 {
+	std::cout << "exercice 1" << std::endl;
 	exercice_one();
+	std::cout << std::endl << "exercice 2" << std::endl;
 	exercice_two();
+	std::cout << std::endl << "exercice 3" << std::endl;
 	exercice_three();
 	return 0;
 }
